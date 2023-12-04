a0ecd3e13f095634af14ec59f97d7a30f16f767b
jorge <jorgeluis199454@gmail.com>
2023-12-04 18:44:00 -0500

    Frond

--- peticiones/src/main/java/com/example/peticiones/Controller/UsuarioController.java
+++ peticiones/src/main/java/com/example/peticiones/Controller/UsuarioController.java
@@ -3,7 +3,6 @@ package com.example.peticiones.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,27 +18,23 @@ import com.example.peticiones.Service.UsuarioService;
 @CrossOrigin("http://localhost:4200")
 
 public class UsuarioController {
-    private final UsuarioService usuarioService;
+  private final UsuarioService usuarioService;
 
-    public UsuarioController(UsuarioService usuarioService) {
-      this.usuarioService = usuarioService;
-    }
+  public UsuarioController(UsuarioService usuarioService) {
+    this.usuarioService = usuarioService;
+  }
 
-    @GetMapping("obtener")
-    public List<Usuario> getAllProducts() {
-        List<Usuario> listUsuario = new ArrayList<>();
-        listUsuario=usuarioService.ObtenerUsuario();
-        return listUsuario;
-    }
-    @PostMapping("agregar")
-    public  Usuario saveAllProducts(@RequestBody Usuario user) {
+  @GetMapping("obtener")
+  public List<Usuario> getAllProducts() {
+    List<Usuario> listUsuario = new ArrayList<>();
+    listUsuario = usuarioService.ObtenerUsuario();
+    return listUsuario;
+  }
 
-        
-       return usuarioService.saveUsuario(user);
-        
-        
-    }
-    
-    
-    
+  @PostMapping("agregar")
+  public Usuario saveAllProducts(@RequestBody Usuario user) {
+    return usuarioService.saveUsuario(user);
+
+  }
+
 }
